21d657aeff7fdac08ee960786745448a58670a53
Dorian De Jesus <dorian.dejesus@ufl.edu>
2025-02-12 12:23:49 -0500

    update 1

---
+++ src/pages/TripPlanning.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TripPlanning = () => {
+  return (
+    <div className="trip-planning">
+      <h1>Trip Planning Page</h1>
+      <p>Start planning your next adventure here!</p>
+    </div>
+  );
+};
+
+export default TripPlanning;
