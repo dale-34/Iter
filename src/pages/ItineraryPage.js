efbb1f89bf7f77813fe2fbd67085caf99f57dfc3
hollymanalansan <100721149+hollymanalansan@users.noreply.github.com>
2025-03-06 17:55:22 -0500

    itenerary page flights & recap

--- src/pages/ItineraryPage.js
+++ src/pages/ItineraryPage.js
@@ -1,41 +1,38 @@
 import React, { useState } from "react";
 import { Header } from "../components/header";
 import { useLocation } from "react-router-dom";
+import Recap from "../components/Recap";
+import Flights from "../components/Flights";
 import ActivityCard from '../components/activityCard';
 import '../css/ItineraryPage.css';
 
 function ItineraryPage() {
     const location = useLocation();
     const { vacationPlan } = location.state || {};
+
     return (
-        <div>
-        <div>
-            <div className="header">
-                <Header />
+        <div className="itinerary-wrapper">
+            {/* Keep the header consistent */}
+            <Header />
+
+            {/* Trip Name Centered Below Header */}
+            <div className="trip-title">
+                <h1>Costa Rica Trip 2025</h1>
             </div>
-            <div>
-                <h3>Your Vacation Plan:</h3>
-                {vacationPlan ? (
-                    <p>{vacationPlan}</p>
-                ) : (
-                    <p>
-                        No vacation plan available. Please go back and create
-                        one.
-                    </p>
-                )}
-            </div>
-        </div>
-        <div className='trip-name'>
-            <h1>Costa Rica Trip 2025</h1>
-        </div>
-        <div className='recap-container'>
-            <h2>Recap</h2>
-            <p>Recap of the information for trip</p>
-        </div>
-        <div className='flights-container'>
-            <h2>Flight Information</h2>
-            <p>Possible flights to be booked</p>
-        </div>
+
+            {/* Content Wrapper to Avoid Header Issues */}
+            <div className="itinerary-content">
+                <div className="vacation-plan">
+                    <h3>Your Vacation Plan:</h3>
+                    {vacationPlan ? <p>{vacationPlan}</p> : <p>No vacation plan available. Please go back and create one.</p>}
+                </div>
+
+                {/* Recap Section */}
+                <Recap />
+
+                {/* Flights Section */}
+                <Flights />
+
         <div className='activity-container'>
             <h2>Activities and Excursions</h2>
             <ActivityCard />
@@ -43,6 +40,7 @@ function ItineraryPage() {
         <div className='food-container'>
             <h2>Food Options</h2>
             <ActivityCard />
+        </div>
         </div>
         </div>
     );
