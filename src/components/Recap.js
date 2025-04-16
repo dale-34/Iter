de7c4ac743dbcc267c75d3a6c5d3dc91025c30c1
Dorian De Jesus <dorian.dejesus@ufl.edu>
2025-04-16 12:17:10 -0400

    recap fixed

--- src/components/Recap.js
+++ src/components/Recap.js
@@ -8,7 +8,11 @@ const Recap = ({ vacationPlan = [], startDate, endDate, destination, budget, sta
             <div className="recap-card">
                 <p><strong>Start Destination:</strong> {startLocation}</p>
                 <p><strong>End Destination:</strong> {destination}</p>
-                <p><strong>Dates:</strong> {startDate.toDateString()} - {endDate.toDateString()}</p>
+                <p><strong>Dates:</strong> 
+                    {(startDate.toISOString().split("T")[0] === "2099-12-31" || endDate.toISOString().split("T")[0] === "2099-12-31") 
+                    ? " Whenever you want" 
+                    : ` ${startDate.toDateString()} - ${endDate.toDateString()}`}
+                </p>
                 <p><strong>Budget:</strong> ${parseInt(budget[0])} - ${parseInt(budget[1])}</p>
                 <p><strong>Weather:</strong> {vacationPlan.vacation.climate} </p>
             </div>
