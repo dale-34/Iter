f7b00d226d402e73bbe9bdb986cffb140b1ec767
Dorian De Jesus <dorian.dejesus@ufl.edu>
2025-04-21 17:45:34 -0400

    destinations updates and housing pictures

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -9,25 +9,25 @@ export const Destinations = ({ onDestinationChange }) => {
 
   // Continent Images
   const continentImages = {
-    "North America": "https://i.gocollette.com/img/destination-page/north-america/north-america-continent/north-america-continent-ms3r.jpg?h=720&w=1280&la=en-CA",
+    "North America": "https://justglobetrotting.com/wp-content/uploads/2017/05/north-america.jpg",
     "Europe": "https://i.gocollette.com/img/destination-page/europe/europe-continent/europe-ms2.jpg?h=720&w=1280&la=en-AU",
     "Asia": "https://t3.ftcdn.net/jpg/01/46/56/78/360_F_146567837_c6Y6q0382DrpEVvVPTvOR1AlvVbC2qmV.jpg",
-    "South America": "https://i.natgeofe.com/n/076afed0-fcd5-4b1e-b585-b598fd8566ad/01_SouthAmerica_3x2.jpg",
-    "Africa": "https://blog.sunsafaris.com/wp-content/uploads/2016/07/WildlifeSunsets.jpg",
+    "South America": "https://media.istockphoto.com/id/930824730/photo/machu-picchu-peru.jpg?s=612x612&w=0&k=20&c=sXUMiy9gRGKow9GHNY_S9BH_ZEekIEcpDHyti5IjZNQ=",
+    "Africa": "https://www.breakingtravelnews.com/images/uploads/tourism/Serengeti-National-Park-Featured.jpg",
     "Australia": "https://images.pexels.com/photos/995764/pexels-photo-995764.jpeg?cs=srgb&dl=pexels-patrick-995764.jpg&fm=jpg",
   };
 
   // Recommendation images for different categories
   const recommendations = {
     Popular: [
-      { title: "Eiffel Tower, Paris", img: "https://www.worldatlas.com/r/w1200/upload/09/04/49/untitled-design-374.jpg" },
-      { title: "Statue of Liberty, NY", img: "https://www.statueoflibertytour.com/wp-content/uploads/2022/05/statue-of-liberty-gaffb1612d_1920.jpg" },
-      { title: "Great Wall, China", img: "https://whc.unesco.org/uploads/thumbs/site_0438_0035-750-750-20241024162522.jpg" },
+      { title: "Eiffel Tower", img: "https://www.travelandleisure.com/thmb/SPUPzO88ZXq6P4Sm4mC5Xuinoik=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/eiffel-tower-paris-france-EIFFEL0217-6ccc3553e98946f18c893018d5b42bde.jpg" },
+      { title: "Statue of Liberty", img: "https://images.squarespace-cdn.com/content/v1/564fe1dfe4b02e24589cb0cd/1645814786032-WNCKTFACB84H9051CJYJ/Cover.png?format=1500w" },
+      { title: "Great Wall of China", img: "https://whc.unesco.org/uploads/thumbs/site_0438_0035-750-750-20241024162522.jpg" },
     ],
     Tropical: [
-      { title: "Bora Bora, French Polynesia", img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/60/b0/bd/bora-bora.jpg?w=1400&h=1400&s=1" },
-      { title: "Maui, Hawaii", img: "https://images.squarespace-cdn.com/content/v1/5d815d4db50c44555a72c530/4eebf383-70c6-4acb-948e-5221326cfbbe/aerial+view+maui+hawaii+mountains+by+the+sea+with+clouds_hawaii+tourism+authority+.jpg" },
-      { title: "Maldives", img: "https://www.zubludiving.com/images/Maldives/General/Diving-Maldives-Budget-banner.jpg" },
+      { title: "Bora Bora, French Polynesia", img: "https://cache.marriott.com/is/image/marriotts7prod/xr-bobxr-overview-overwater-villa--23952:Feature-Hor?wid=1920&fit=constrain" },
+      { title: "Maui, Hawaii", img: "https://www.theknot.com/tk-media/images/6231de02-db4d-4eda-b37e-e3379a83b0a9" },
+      { title: "Puerto Rico", img: "https://caribbeanbreezeadventures.com/wp-content/uploads/sites/7128/2024/01/AdobeStock_544504039-scaled.jpeg?resize=360%2C240&zoom=2" },
     ],
     Cityscape: [
       { title: "Tokyo, Japan", img: "https://media.cntraveler.com/photos/678166b8a51cc7c3458df25a/16:9/w_2580,c_limit/pexels-Aleksandar%20Pasaric-2506923.jpg" },
@@ -57,7 +57,7 @@ export const Destinations = ({ onDestinationChange }) => {
       {!showSpecificPlaces ? (
         <>
           <h2 className="destinations__title">Where are you looking to go?</h2>
-
+          <p className="destinations__subtitle">Select a continent, popular destination, or input your own destination.</p>
           {/* 🌍 Continent Selection */}
           <div className="destinations__grid">
             {Object.keys(continentImages).map((continent) => (
@@ -67,7 +67,7 @@ export const Destinations = ({ onDestinationChange }) => {
                 onClick={() => handleContinentClick(continent)}
                 style={{
                   backgroundImage: `url(${continentImages[continent]})`,
-                  filter: selectedDestination === continent ? "none" : "grayscale(50%)",
+                  filter: selectedDestination === continent ? "none" : "grayscale(0%)",
                 }}
               >
                 {continent}
